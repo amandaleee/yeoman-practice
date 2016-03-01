31cf0c1704328f6e3e60760e0ff29b0c5ae3cdf5
amandaleee <amandalee@amandalee.org>
2016-02-29 23:47:05 -0600

    updates to the app - it's functional now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,15 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name yeomanPracticeApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the yeomanPracticeApp
- */
+
 angular.module('yeomanPracticeApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function($scope) {
+    $scope.items = ['item 1', 'item 2', 'item 3'];
+    $scope.addItem = function(){
+    	$scope.items.push($scope.item); //adds new item to list - this has to be
+    	$scope.item = ''; //resets the 'item' input to blank
+    	//come back to this with error messaging/not submitting empty stuff
+    };
+    $scope.removeItem = function(index){
+    	$scope.items.splice(index, 1); //must be items - not item.
+    };
   });
