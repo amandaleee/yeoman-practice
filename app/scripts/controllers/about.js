8a45c42ec8452996acd0d92c64f7a89031289f9a
amandaleee <amandalee@amandalee.org>
2016-02-29 23:06:53 -0600

    updates

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanPracticeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yeomanPracticeApp
+ */
+angular.module('yeomanPracticeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yeomanPracticeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yeomanPracticeApp
+ */
+angular.module('yeomanPracticeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
